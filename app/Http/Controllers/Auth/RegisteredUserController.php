96a51406b33e43cd6822b6d79d67e5a9069533ec
josephmiguelchan <josephmiguel.chan@benilde.edu.ph>
2022-07-13 00:52:31 +0800

    Added more fields on User Creation

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -8,6 +8,7 @@ use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 
@@ -33,20 +34,41 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request)
     {
+        # Validate Form First
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:20', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', 'confirmed', 'max:20', Rules\Password::defaults()],
         ]);
 
+        # Eloquent ORM User Creation (Does not support assigment of Guarded fields)
+        /*
         $user = User::create([
             'name' => $request->name,
+            'username' => $request->username,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'role' => 'Charity Admin',
         ]);
+        */
 
+        # Create New Charitable Organization Record
+
+        # Create a New User Record
+        $user = new User;
+        $user->name = $request->name;
+        $user->username = $request->username;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->role = 'Charity Admin';
+        $user->status = 'Pending';
+        $user->save();
+
+        # Create a New Event (registration)
         event(new Registered($user));
 
+        # Automatically Logging in of user
         Auth::login($user);
 
         return redirect(RouteServiceProvider::HOME);
