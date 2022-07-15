192427b6709d632238e4803d0be7a10896ae8e1d
josephmiguelchan <josephmiguel.chan@benilde.edu.ph>
2022-07-16 00:16:19 +0800

    Updated forgot password page + change password msg

--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
+    'reset' => 'Your password has been successfully reset.',
+    'sent' => 'Password reset link has been sent to your email.',
+    'throttled' => 'Please wait before retrying again.',
     'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'user' => "Sorry, email address not found.",
 
 ];
