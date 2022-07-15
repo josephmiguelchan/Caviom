19e070c2cb1c9b3c131cc06a273a5c07ba11dbe5
josephmiguelchan <josephmiguel.chan@benilde.edu.ph>
2022-07-15 12:13:02 +0800

    Changed page name and favicon

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,14 +4,13 @@
 <head>
 
     <meta charset="utf-8" />
-    <title>Register | Miguellumbao - Admin & Dashboard Template</title>
+    <title>Caviom — Register | Charity Management System</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{ asset('backend/assets/images/Caviom Logo.png') }}">
     <!-- Bootstrap Css -->
-    <link href="{{ asset('backend/assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet"
-        type="text/css" />
+    <link href="{{ asset('backend/assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
     <!-- Icons Css -->
     <link href="{{ asset('backend/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
     <!-- App Css-->
