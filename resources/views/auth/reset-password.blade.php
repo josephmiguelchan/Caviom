21012d730cbd0a9196a472368d28de129a823235
josephmiguelchan <josephmiguel.chan@benilde.edu.ph>
2022-07-18 01:33:47 +0800

    Updated UI of Reset Password Page

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,48 +1,87 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('auth_master')
+@section('title', 'Reset Password')
+@section('auth')
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+<div class="container-fluid p-0" style="max-width: 490px">
+    <div class="card">
+        <div class="card-body">
 
-        <form method="POST" action="{{ route('password.update') }}">
-            @csrf
 
-            <!-- Password Reset Token -->
-            <input type="hidden" name="token" value="{{ $request->route('token') }}">
+            <div class="p-3">
+                <form method="post" action="{{ route('password.update') }}">
+                    @csrf
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
+                    <h1 style="color: #62896d" class="mt-3"><strong>PASSWORD RESET</strong></h1>
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
+                    <!-- Password Reset Token -->
+                    <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+                    <!-- Email Address -->
+                    <div class="mb-4">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control" name="email" id="email"
+                            value="{{ old('email', $request->email) }}" readonly>
+                        @error('confirm_password')
+                            <div class="text-danger">
+                                <i class="mdi mdi-alert-outline me-2"></i>
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+
+                    <!-- Password -->
+                    <div class="mb-4">
+                        <label for="password" class="form-label">
+                            New Password
+                            <span data-bs-toggle="tooltip" data-bs-placement="right"
+                                title="Password must be 8-20 characters with a letter and a number."
+                                data-bs-original-title="yes">
+                                <i class="mdi mdi-information-outline"></i>
+                            </span>
+                        </label>
+                        <input type="password" class="form-control" name="password" id="password"
+                            placeholder="New Password">
+                        @error('password')
+                            <div class="text-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+
+                    <!-- Confirm Password -->
+                    <div>
+                        <label for="password_confirmation" class="form-label">Confirm Password</label>
+                        <input type="password" class="form-control" name="password_confirmation"
+                            id="password_confirmation" placeholder="Confirm Password">
+                        @error('password_confirmation')
+                            <div class="text-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+
+                    <div class="@unless($errors->has('email') || Session::has('status'))mt-5 @endunless">
+                        @error ('email')
+                            <!-- Send Verification Link Failed message -->
+                            <div class="text-danger text-center my-3">
+                                {{$message}}
+                            </div>
+                        @enderror
+                        @if(Session::has('status'))
+                            <div class="text-info text-center my-3">
+                                {{ Session::get('status') }}
+                            </div>
+                        @endif
+                    </div>
+
+                    <input type="submit" class="btn btn-dark btn-rounded w-100 waves-effect list-inline-item"
+                        value="Save Password">
+                </form>
             </div>
+        </div>
+        <!-- end cardbody -->
+    </div>
+    <!-- end card -->
+</div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                    type="password"
-                                    name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Reset Password') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+@endsection
