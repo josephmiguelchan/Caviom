c6d71aef06ad4bc414a35c2fb3777a57a50d6aa7
josephmiguelchan <josephmiguel.chan@benilde.edu.ph>
2022-07-18 17:36:49 +0800

    Forget password reset will now throw toastr

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -54,11 +54,16 @@ class NewPasswordController extends Controller
             }
         );
 
+        $notification = array(
+            'message' => 'Password has been reset successfully! Kindly login again to continue.',
+            'alert-type' => 'success',
+        );
+
         // If the password was successfully reset, we will redirect the user back to
         // the application's home authenticated view. If there is an error we can
         // redirect them back to where they came from with their error message.
         return $status == Password::PASSWORD_RESET
-            ? redirect()->route('login')->with('status', __($status))
+            ? redirect()->route('login')->with($notification)
             : back()->withInput($request->only('email'))
             ->withErrors(['email' => __($status)]);
     }
