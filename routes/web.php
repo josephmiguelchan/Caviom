35508027a86bfaab22614b7486c763e7cb0ae097
josephmiguelchan <josephmiguel.chan@benilde.edu.ph>
2022-08-07 22:31:32 +0800

    Simplified / universified route groups

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/', function () {
 //     return view('charity.index');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
+
 # Charity Group Controller
 Route::controller(CharityController::class)->group(function () {
 
@@ -44,112 +45,135 @@ Route::controller(CharityController::class)->group(function () {
     Route::get('/user/logout', 'destroy')->name('user.logout');
 });
 
-# Donors and Donations Group Controller
-// Route::controller(DonorController::class)->group(function() {
-Route::prefix('/donors')->middleware(['auth', 'verified'])->group(function () {
 
-    # Leads
-    Route::get('/leads', function () {
-        return view('charity.donors.leads.all');
-    })->name('leads.all');
-    Route::get('/leads/9a7445e2-07eb-11ed-861d-0242ac120002', function () {
-        return view('charity.donors.leads.view');
-    })->name('leads.view');
-    // Route::get('/leads/delete/1', deleteLead)->name('leads.delete');
+# Charity Users Group
+Route::prefix('/charity')->group(function () {
+    # Donors and Donations Group Controller
+    // Route::controller(DonorController::class)->group(function() {
+    Route::prefix('/donors')->middleware(['auth', 'verified'])->group(function () {
 
-    # Prospects
-    Route::get('/prospects', function () {
-        return view('charity.donors.prospects.all');
-    })->name('prospects.all');
-    Route::get('/prospects/93e5c76a-2316-46e4-b24f-b33131100457', function () {
-        return view('charity.donors.prospects.view');
-    })->name('prospects.view');
-    // Route::get('/prospects/move/1', moveLead)->name('prospects.move');
+        # Leads
+        Route::get('/leads', function () {
+            return view('charity.donors.leads.all');
+        })->name('leads.all');
+        Route::get('/leads/9a7445e2-07eb-11ed-861d-0242ac120002', function () {
+            return view('charity.donors.leads.view');
+        })->name('leads.view');
+        // Route::get('/leads/delete/1', deleteLead)->name('leads.delete');
+
+        # Prospects
+        Route::get('/prospects', function () {
+            return view('charity.donors.prospects.all');
+        })->name('prospects.all');
+        Route::get('/prospects/93e5c76a-2316-46e4-b24f-b33131100457', function () {
+            return view('charity.donors.prospects.view');
+        })->name('prospects.view');
+        // Route::get('/prospects/move/1', moveLead)->name('prospects.move');
+    });
+    // });
+
+    # Our Charitable Organization Controller
+    // Route::controller(OurCharityOrgController::class)->group(function() {
+    Route::name('charity.')->prefix('/our-charity')->middleware(['auth', 'verified'])->group(function () {
+
+        # Public Profile - Only Charity Admins can access the ff:
+        Route::name('profile')->prefix('/profile')->middleware(['charity.admin'])->group(function () {
+            Route::get('', function () {
+                return view('charity.main.profile.index');
+            });
+            Route::get('/setup', function () {
+                return view('charity.main.profile.setup');
+            })->name('.setup');
+            Route::get('/apply-for-verification', function () {
+                return view('charity.main.profile.verify');
+            })->name('.verify');
+        });
+
+        # Projects
+        Route::name('projects')->prefix('/projects')->group(function () {
+            Route::get('', function () {
+                return view('charity.main.projects.all');
+            });
+            Route::get('/add', function () {
+                return view('charity.main.projects.add');
+            })->name('.add');
+            Route::get('/1a2267d9-3f39-4ef7-b6aa-5884f6b8e606', function () {
+                return view('charity.main.projects.view');
+            })->name('.view');
+        });
+
+
+        # Users
+        Route::name('users')->prefix('/users')->group(function () {
+            Route::get('', function () {
+                return view('charity.main.users.all');
+            });
+            Route::middleware(['charity.admin'])->get('/add', function () {
+                return view('charity.main.users.add');
+            })->name('.add');
+            // To add - Route::get() for editing email address of pending accounts with Charity Admin access only.
+            // To add - Route::get() for deleting pending user accounts permanently with Charity Admin access only.
+            Route::get('/6a9ae42b-f01e-4b69-a074-7ec7933557fd', function () {
+                return view('charity.main.users.view');
+            })->name('.view');
+        });
+
+
+        # Beneficiaries
+        Route::name('beneficiaries')->prefix('/beneficiaries')->group(function () {
+            Route::get('', function () {
+                return view('charity.main.beneficiaries.all');
+            });
+            Route::get('/add', function () {
+                return view('charity.main.beneficiaries.add');
+            })->name('.add');
+            Route::get('/69a60048-d093-41d7-bf58-d620ec99c979', function () {
+                return view('charity.main.beneficiaries.view');
+            })->name('.view');
+            Route::get('/edit/69a60048-d093-41d7-bf58-d620ec99c979', function () {
+                return view('charity.main.beneficiaries.edit');
+            })->name('.edit');
+            Route::post('/save/69a60048-d093-41d7-bf58-d620ec99c979', function () {
+            })->name('.update');
+            // To add - Route::get() for deleting individual beneficiary records.
+        });
+
+
+        # Benefactors
+        Route::name('benefactors')->prefix('/benefactors')->group(function () {
+            Route::get('', function () {
+                return view('charity.main.benefactors.all');
+            });
+            Route::get('/add', function () {
+                return view('charity.main.benefactors.add');
+            })->name('.add');
+            Route::get('/6e4a560c-1252-11ed-861d-0242ac120002', function () {
+                return view('charity.main.benefactors.view');
+            })->name('.view');
+            Route::get('/edit/6e4a560c-1252-11ed-861d-0242ac120002', function () {
+                return view('charity.main.benefactors.edit');
+            })->name('.edit');
+            // To add - Route::get() for deleting individual benefactor records.
+        });
+
+
+        # Volunteers
+        Route::name('volunteers')->prefix('/volunteers')->group(function () {
+            Route::get('', function () {
+                return view('charity.main.volunteers.all');
+            });
+            Route::get('/add', function () {
+                return view('charity.main.volunteers.add');
+            })->name('.add');
+            Route::get('/7ba0c587-d347-4bcf-9e0e-28ec06066fb0', function () {
+                return view('charity.main.volunteers.view');
+            })->name('.view');
+            Route::get('/edit/7ba0c587-d347-4bcf-9e0e-28ec06066fb0', function () {
+                return view('charity.main.volunteers.edit');
+            })->name('.edit');
+        });
+    });
+    // });
 });
-// });
-
-# Our Charitable Organization Controller
-// Route::controller(DonorController::class)->group(function() {
-Route::prefix('/our-charity')->middleware(['auth', 'verified'])->group(function () {
-
-    # Public Profile
-    Route::get('/profile', function () {
-        return view('charity.main.profile.index');
-    })->name('charity.profile');
-    Route::get('/profile/setup', function () {
-        return view('charity.main.profile.setup');
-    })->name('charity.profile.setup');
-    Route::get('/profile/apply-for-verification', function () {
-        return view('charity.main.profile.verify');
-    })->name('charity.profile.verify');
-
-    # Projects
-    Route::get('/projects', function () {
-        return view('charity.main.projects.all');
-    })->name('charity.projects');
-    Route::get('/projects/add', function () {
-        return view('charity.main.projects.add');
-    })->name('charity.projects.add');
-
-
-    # Users
-    Route::get('/users', function () {
-        return view('charity.main.users.all');
-    })->name('charity.users');
-    Route::get('/users/add', function () {
-        return view('charity.main.users.add');
-    })->name('charity.users.add');
-    Route::get('/users/6a9ae42b-f01e-4b69-a074-7ec7933557fd', function () {
-        return view('charity.main.users.view');
-    })->name('charity.users.view');
-
-
-    # Beneficiaries
-    Route::get('/beneficiaries', function () {
-        return view('charity.main.beneficiaries.all');
-    })->name('charity.beneficiaries');
-    Route::get('/beneficiaries/add', function () {
-        return view('charity.main.beneficiaries.add');
-    })->name('charity.beneficiaries.add');
-    Route::get('/beneficiaries/69a60048-d093-41d7-bf58-d620ec99c979', function () {
-        return view('charity.main.beneficiaries.view');
-    })->name('charity.beneficiaries.view');
-    Route::get('/beneficiaries/edit/69a60048-d093-41d7-bf58-d620ec99c979', function () {
-        return view('charity.main.beneficiaries.edit');
-    })->name('charity.beneficiaries.edit');
-    Route::post('/beneficiaries/save/69a60048-d093-41d7-bf58-d620ec99c979', function () {
-    })->name('charity.beneficiaries.update');
-
-
-    # Benefactors
-    Route::get('/benefactors', function () {
-        return view('charity.main.benefactors.all');
-    })->name('charity.benefactors');
-    Route::get('/benefactors/add', function () {
-        return view('charity.main.benefactors.add');
-    })->name('charity.benefactors.add');
-    Route::get('/benefactors/6e4a560c-1252-11ed-861d-0242ac120002', function () {
-        return view('charity.main.benefactors.view');
-    })->name('charity.benefactors.view');
-    Route::get('/benefactors/edit/6e4a560c-1252-11ed-861d-0242ac120002', function () {
-        return view('charity.main.benefactors.edit');
-    })->name('charity.benefactors.edit');
-
-
-    # Volunteers
-    Route::get('/volunteers', function () {
-        return view('charity.main.volunteers.all');
-    })->name('charity.volunteers');
-    Route::get('/volunteers/add', function () {
-        return view('charity.main.volunteers.add');
-    })->name('charity.volunteers.add');
-    Route::get('/volunteers/7ba0c587-d347-4bcf-9e0e-28ec06066fb0', function () {
-        return view('charity.main.volunteers.view');
-    })->name('charity.volunteers.view');
-    Route::get('/volunteers/edit/7ba0c587-d347-4bcf-9e0e-28ec06066fb0', function () {
-        return view('charity.main.volunteers.edit');
-    })->name('charity.volunteers.edit');
-});
-// });
 
 require __DIR__ . '/auth.php';
