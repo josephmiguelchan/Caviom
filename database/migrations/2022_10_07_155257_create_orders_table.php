ed6f54f9c7167a8f88793b3d5c1fef3ae62b9491
Justinchua-L <justin.chua@benilde.edu.ph>
2022-10-07 20:23:50 +0800

    Initialized orders (Model + Migration)

---
+++ database/migrations/2022_10_07_155257_create_orders_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('code');
+            $table->foreignId('charitable_organization_id')->constrained();
+            $table->unsignedBigInteger('reference_no',20);
+            $table->string('proof_of_payment',128);
+            $table->char('mode_of_payment',128);
+            $table->float('total', 5, 2)->nullable();
+            $table->char('status',128)->default('Pending');
+            $table->string('remarks_subject',128)->nullable();
+            $table->string('remarks_message',512)->nullable();
+            $table->timestamp('status_updated_at')->nullable();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('paid_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
