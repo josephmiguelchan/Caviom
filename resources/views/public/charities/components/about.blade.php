4b932a9f0e8e5490029c37fdba979ee7e794393e
josephmiguelchan <josephmiguel.chan@benilde.edu.ph>
2022-10-07 12:25:08 +0800

    Started Public Page

---
+++ resources/views/public/charities/components/about.blade.php
@@ -0,0 +1,226 @@
+<!-- Title -->
+{{-- <div class="text-center my-5">
+    <h1 class="display-1 fw-bold">About Us</h1>
+    <p class="text-muted">San Roque United, Inc.</p>
+</div> --}}
+
+<!-- Our Story -->
+<div class="row align-items-center">
+    <div class="col-md-6 p-5">
+        <h1 class="display-3 my-3">Our Story</h1>
+        <p>
+            Raw denim you probably haven't heard of them jean shorts Austin.
+            Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache
+            cliche tempor, williamsburg carles vegan helvetica.
+        </p>
+        <p class="mt-2">
+            Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi,
+            qui irure terry richardson ex squid. Aliquip placeat salvia cillum
+            iphone. Seitan aliquip quis cardigan american apparel, butcher
+            voluptate nisi qui.
+        </p>
+    </div>
+    <div class="col-md-6 text-center">
+        <img src="{{ asset('backend/assets/images/small/img-2.jpg') }}" class="img-fluid rounded" alt="Responsive image">
+    </div>
+</div>
+
+<!-- Our Goal -->
+<div class="row align-items-center">
+    <div class="col-md-6 text-center">
+        <img src="{{ asset('backend/assets/images/small/img-1.jpg') }}" class="img-fluid rounded" alt="Responsive image">
+    </div>
+    <div class="col-md-6 p-5">
+        <h1 class="display-3 my-3">Our Goal</h1>
+        <p class="mb-0">
+            Raw denim you probably haven't heard of them jean shorts Austin.
+            Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache
+            cliche tempor, williamsburg carles vegan helvetica. Reprehenderit
+            butcher retro.
+        </p>
+        <ul class="mt-2">
+            <li>Magaling</li>
+            <li>Mahusay</li>
+            <li>Wais</li>
+        </ul>
+        <p> dreamcatcher synth. Cosby sweater eu banh mi,
+            qui irure terry richardson ex squid. Aliquip placeat salvia cillum
+            iphone. Seitan aliquip quis cardigan american apparel, butcher
+            voluptate nisi qui.
+        </p>
+    </div>
+</div>
+
+<!-- Our Team -->
+<h1 class="display-3 mt-5 text-center">The Team</h1>
+{{-- <div class="row justify-content-center my-4" data-masonry='{"percentPosition": true }'>
+    <div class="col-lg-4 col-sm-6">
+        <div class="card m-b-30">
+            <div class="card-body">
+
+                <div class="d-flex align-items-center">
+                    <img class="d-flex me-3 rounded-circle img-thumbnail avatar-lg" src="{{ asset('backend/assets/images/users/avatar-1.jpg') }}" alt="Generic placeholder image">
+                    <div class="flex-grow-1">
+                        <h5 class="mt-0 font-size-18 mb-1">Peter F. Costanzo</h5>
+                        <p class="text-muted font-size-14">Webdeveloper</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div> <!-- end col -->
+    <div class="col-lg-4 col-sm-6">
+        <div class="card m-b-30">
+            <div class="card-body">
+
+                <div class="d-flex align-items-center">
+                    <img class="d-flex me-3 rounded-circle img-thumbnail avatar-lg" src="{{ asset('backend/assets/images/users/avatar-2.jpg') }}" alt="Generic placeholder image">
+                    <div class="flex-grow-1">
+                        <h5 class="mt-0 font-size-18 mb-1">John B. Roman</h5>
+                        <p class="text-muted font-size-14">Webdeveloper</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div> <!-- end col -->
+    <div class="col-lg-4 col-sm-6">
+        <div class="card m-b-30">
+            <div class="card-body">
+
+                <div class="d-flex align-items-center">
+                    <img class="d-flex me-3 rounded-circle img-thumbnail avatar-lg" src="{{ asset('backend/assets/images/users/avatar-3.jpg') }}" alt="Generic placeholder image">
+                    <div class="flex-grow-1">
+                        <h5 class="mt-0 font-size-18 mb-1">Pauline I. Bird</h5>
+                        <p class="text-muted font-size-14">Webdeveloper</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div> <!-- end col -->
+    <div class="col-lg-4 col-sm-6">
+        <div class="card m-b-30">
+            <div class="card-body">
+
+                <div class="d-flex align-items-center">
+                    <img class="d-flex me-3 rounded-circle img-thumbnail avatar-lg" src="{{ asset('backend/assets/images/users/avatar-4.jpg') }}" alt="Generic placeholder image">
+                    <div class="flex-grow-1">
+                        <h5 class="mt-0 font-size-18 mb-1">Ralph L. Alva</h5>
+                        <p class="text-muted font-size-14">Webdeveloper</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div> <!-- end col -->
+</div> --}}
+
+
+<div class="row justify-content-center my-4"> <!-- data-masonry='{"percentPosition": true }' -->
+    <div class="col-sm-6 col-lg-2">
+        <div class="card border border-dark">
+            <div class="text-center my-4">
+                <img src="{{asset('backend/assets/images/users/avatar-1.jpg')}}" class="avatar-lg rounded-circle" alt="Team no. 1">
+                <div class="card-body">
+                    <h5 class="card-title">Pangilinan, J.</h5>
+                    <p class="card-text">Secretary</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6 col-lg-2">
+        <div class="card border border-dark">
+            <div class="text-center my-4">
+                <img src="{{asset('backend/assets/images/users/avatar-4.jpg')}}" class="avatar-lg rounded-circle" alt="Team no. 1">
+                <div class="card-body">
+                    <h5 class="card-title">Magsino, M.</h5>
+                    <p class="card-text">President</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6 col-lg-2">
+        <div class="card border border-dark">
+            <div class="text-center my-4">
+                <img src="{{asset('backend/assets/images/users/avatar-2.jpg')}}" class="avatar-lg rounded-circle" alt="Team no. 1">
+                <div class="card-body">
+                    <h5 class="card-title">Liwanag, C.</h5>
+                    <p class="card-text">Treasurer</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6 col-lg-2">
+        <div class="card border border-dark">
+            <div class="text-center my-4">
+                <img src="{{asset('backend/assets/images/users/avatar-3.jpg')}}" class="avatar-lg rounded-circle" alt="Team no. 1">
+                <div class="card-body">
+                    <h5 class="card-title">Magsakay, J.</h5>
+                    <p class="card-text">Social Worker</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6 col-lg-2 text-center">
+        <div class="card border border-dark">
+            <div class="text-center my-4">
+                <img src="{{asset('upload/avatar_img/no_avatar.png')}}" class="avatar-lg rounded-circle" alt="Team no. 1">
+                <div class="card-body">
+                    <h5 class="card-title">+4 more...</h5>
+                    <p class="card-text">Charity Users</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end row -->
+
+<!-- Awards -->
+<h1 class="display-3 mt-5 text-center">Certificates & Recognition</h1>
+<div class="row text-center">
+    <div id="carouselExampleIndicators2" class="carousel slide" data-bs-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="0" class="active"></li>
+            <li data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="1"></li>
+            <li data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="2"></li>
+            <li data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="3"></li>
+            <li data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="4"></li>
+        </ol>
+        <div class="carousel-inner " role="listbox">
+            <div class="carousel-item active">
+                <img class="d-block img-fluid" style="width: 100%; height: 21vh; object-fit: cover; background: rgb(191,255,199);
+                    background: linear-gradient(347deg, rgba(191,255,199,1) 0%, rgba(24,165,167,1) 100%);">
+                <div class="carousel-caption align-text-center text-white-50">
+                    <a href="#">
+                        <h1>Outstanding Foundation of 2022 Awarded by Association of Charitable Organizations in the Philippines</h1>
+                    </a>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="align-items-center" style="width: 100%; height: 40vh; object-fit: cover;"
+                    src="{{ asset('backend/assets/images/small/img-2.jpg') }}" alt="Second cover photo">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block img-fluid" style="width: 100%; height: 40vh; object-fit: cover;"
+                    src="{{ asset('backend/assets/images/small/img-3.jpg') }}" alt="Third cover photo">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block img-fluid" style="width: 100%; height: 40vh; object-fit: cover;"
+                    src="{{ asset('backend/assets/images/small/img-4.jpg') }}" alt="Fourth cover photo">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block img-fluid" style="width: 100%; height: 40vh; object-fit: cover;"
+                    src="{{ asset('backend/assets/images/small/img-5.jpg') }}" alt="Fifth cover photo">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators2" role="button" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators2" role="button" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
---
+++ resources/views/public/public_master.blade.php
@@ -0,0 +1,259 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8" />
+    <title>Caviom — @yield('title') | Charity Management System</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('backend/assets/images/Caviom Logo.png') }}">
+
+    <!-- Toastr Css-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('backend/assets/libs/toastr/build/toastr.min.css') }}">
+
+    <!-- jquery.vectormap css -->
+    <link href="{{ asset('backend/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css') }}" rel="stylesheet" type="text/css" />
+
+    <!-- DataTables -->
+    <link href="{{ asset('backend/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('backend/assets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('backend/assets/libs/datatables.net-select-bs4/css//select.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+
+    <!-- Responsive datatable examples -->
+    <link href="{{ asset('backend/assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+
+    <!-- ION Slider -->
+    <link href="{{ asset('backend/assets/libs/ion-rangeslider/css/ion.rangeSlider.min.css') }}" rel="stylesheet"
+        type="text/css" />
+
+    <!-- Sweet Alert-->
+    <link href="{{ asset('backend/assets/libs/sweetalert2/sweetalert2.min.css') }}" rel="stylesheet" type="text/css">
+
+    <!-- Lightbox css -->
+    <link href="{{ asset('backend/assets/libs/magnific-popup/magnific-popup.css') }}" rel="stylesheet" type="text/css">
+
+    <!-- Advanced Forms Css -->
+    <link href="{{ asset('backend/assets/libs/select2/css/select2.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('backend/assets/libs/bootstrap-datepicker/css/bootstrap-datepicker.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/assets/libs/spectrum-colorpicker2/spectrum.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('backend/assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css') }}" rel="stylesheet">
+
+    <!-- Tags Input Css -->
+    <link href="{{ asset('backend/assets/tagsinput/src/bootstrap-tagsinput.css') }}" rel="stylesheet" type="text/css">
+
+    <!-- Bootstrap Css -->
+    <link href="{{ asset('backend/assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+
+    <!-- Icons Css -->
+    <link href="{{ asset('backend/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+
+    <!-- Dropzone Css -->
+    <link href="{{ asset('backend/assets/libs/dropzone/min/dropzone.min.css') }}" rel="stylesheet" type="text/css" />
+
+    <!-- twitter-bootstrap-wizard css -->
+    <link rel="stylesheet" href="{{ asset('backend/assets/libs/twitter-bootstrap-wizard/prettify.css') }}">
+
+    <!-- App Css-->
+    <link href="{{ asset('backend/assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body data-topbar="dark"  style="background-color: #f7f3ea">
+    {{-- <div class="bg-overlay"></div> --}}
+
+    <!-- Loader -->
+    {{-- <div id="preloader">
+        <div id="status">
+            <div class="spinner">
+                <i class="ri-loader-line spin-icon"></i>
+            </div>
+        </div>
+    </div> --}}
+
+    <!-- Header -->
+    @include('charity.body.header')
+
+
+    <!-- Main Content -->
+    <div class="page-content p-0" >
+        @yield('main')
+
+        <!-- Footer -->
+        <footer class="footer" style="left: 0%">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-6">
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="text-sm-end d-none d-sm-block">
+                            ©
+                            <script>
+                                document.write(new Date().getFullYear())
+                            </script> Caviom. All rights reserved.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- end Footer -->
+
+    </div>
+    <!-- end Main Content -->
+
+
+    <!-- JAVASCRIPT -->
+    <script src="{{ asset('backend/assets/libs/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/metismenu/metisMenu.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/simplebar/simplebar.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/node-waves/waves.min.js') }}"></script>
+
+    <!-- apexcharts -->
+    <script src="{{ asset('backend/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
+
+    <!-- jquery.vectormap map -->
+    <script src="{{ asset('backend/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-us-merc-en.js') }}"></script>
+
+    <!-- Required datatable js -->
+    <script src="{{ asset('backend/assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+
+    <!-- Buttons examples -->
+    <script src="{{ asset('backend/assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/jszip/jszip.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/pdfmake/build/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/pdfmake/build/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/datatables.net-buttons/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/datatables.net-buttons/js/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/datatables.net-buttons/js/buttons.colVis.min.js') }}"></script>
+
+    <script src="{{ asset('backend/assets/libs/datatables.net-keytable/js/dataTables.keyTable.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/datatables.net-select/js/dataTables.select.min.js') }}"></script>
+
+    <!-- Responsive examples -->
+    <script src="{{ asset('backend/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js') }}">
+    </script>
+
+    <!-- Datatable init js -->
+    <script src="{{ asset('backend/assets/js/pages/datatables.init.js') }}"></script>
+
+    <!-- Dashboard init js -->
+    <script src="{{ asset('backend/assets/js/pages/dashboard.init.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/pages/caviom-dashboard.init.js') }}"></script>
+
+    <!-- App js -->
+    <script src="{{ asset('backend/assets/js/app.js') }}"></script>
+
+    <!-- toastr plugin -->
+    <script src="{{ asset('backend/assets/libs/toastr/build/toastr.min.js') }}"></script>
+
+    <!-- toastr init -->
+    <script src="{{ asset('backend/assets/js/pages/toastr.init.js') }}"></script>
+
+    <!-- Magnific Popup-->
+    <script src="{{ asset('backend/assets/libs/magnific-popup/jquery.magnific-popup.min.js') }}"></script>
+
+    <!-- lightbox init js-->
+    <script src="{{ asset('backend/assets/js/pages/lightbox.init.js') }}"></script>
+
+    <!--tinymce js-->
+    <script src="{{ asset('backend/assets/libs/tinymce/tinymce.min.js') }}"></script>
+
+    <!-- init js -->
+    <script src="{{ asset('backend/assets/js/pages/form-editor.init.js') }}"></script>
+
+    <!-- Ion Range Slider-->
+    <script src="{{ asset('backend/assets/libs/ion-rangeslider/js/ion.rangeSlider.min.js') }}"></script>
+
+    <!-- Range slider init js-->
+    <script src="{{ asset('backend/assets/js/pages/range-sliders.init.js') }}"></script>
+
+    <!-- Sweet Alerts js -->
+    <script src="{{ asset('backend/assets/libs/sweetalert2/sweetalert2.min.js') }}"></script>
+
+    <!-- Sweet alert init js-->
+    <script src="{{ asset('backend/assets/js/pages/sweet-alerts.init.js') }}"></script>
+
+    <!-- Tags Input js-->
+    <script src="{{ asset('backend/assets/tagsinput/src/bootstrap-tagsinput.js') }}"></script>
+
+    <!-- Validation js-->
+    <script src="{{ asset('backend/assets/js/validate.min.js') }}"></script>
+
+    <!-- Dropzone js-->
+    <script src="{{ asset('backend/assets/libs/dropzone/min/dropzone.min.js') }}"></script>
+
+    <!-- twitter-bootstrap-wizard js -->
+    <script src="{{ asset('backend/assets/libs/twitter-bootstrap-wizard/jquery.bootstrap.wizard.min.js') }}"></script>
+
+    <script src="{{ asset('backend/assets/libs/twitter-bootstrap-wizard/prettify.js') }}"></script>
+
+    <!-- form wizard init -->
+    <script src="{{ asset('backend/assets/js/pages/form-wizard.init.js') }}"></script>
+
+    <!-- masonry pkgd -->
+    <script src="{{ asset('backend/assets/libs/masonry-layout/masonry.pkgd.min.js') }}"></script>
+
+    <!-- Advanced Forms -->
+    <script src="{{ asset('backend/assets/libs/select2/js/select2.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/bootstrap-datepicker/js/bootstrap-datepicker.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/spectrum-colorpicker2/spectrum.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/admin-resources/bootstrap-filestyle/bootstrap-filestyle.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/bootstrap-maxlength/bootstrap-maxlength.min.js') }}"></script>
+
+    <!-- form mask -->
+    <script src="{{ asset('backend/assets/libs/inputmask/jquery.inputmask.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/pages/form-mask.init.js') }}"></script>
+
+    <script src="{{ asset('backend/assets/js/pages/form-advanced.init.js') }}"></script>
+
+    <script>
+        @if (Session::has('message'))
+            var type = "{{ Session::get('alert-type', 'info') }}"
+
+            toastr.options = {
+                "closeButton": true,
+                "debug": false,
+                "newestOnTop": true,
+                "progressBar": false,
+                "positionClass": "toast-top-center",
+                "preventDuplicates": true,
+                "onclick": null,
+                "showDuration": 500,
+                "hideDuration": 1000,
+                "timeOut": 5000,
+                "extendedTimeOut": 1500,
+                "showEasing": "swing",
+                "hideEasing": "linear",
+                "showMethod": "slideDown",
+                "hideMethod": "fadeOut"
+            }
+
+            switch (type) {
+                case 'info':
+                    toastr.info(" {{ Session::get('message') }} ", "{{ Session::get('title') }}");
+                    break;
+
+                case 'success':
+                    toastr.success(" {{ Session::get('message') }} ", "{{ Session::get('title') }}");
+                    break;
+
+                case 'warning':
+                    toastr.warning(" {{ Session::get('message') }} ", "{{ Session::get('title') }}");
+                    break;
+
+                case 'error':
+                    toastr.error(" {{ Session::get('message') }} ", "{{ Session::get('title') }}");
+                    break;
+            }
+        @endif
+    </script>
+</body>
+
+</html>
