eb41e3329e5bebe2edfdbe4f951db6c2c7d16afd
Justinchua-L <justin.chua@benilde.edu.ph>
2022-10-12 00:24:42 +0800

    Manage Orders Root Admin Done

---
+++ app/Models/Admin/order_items.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class order_items extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+    public $timestamps = false;
+
+
+}
